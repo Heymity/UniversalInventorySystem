12437aa17909e2756e3c5d90b7079d483aa51130
Gabriel Pasquale <gabriel.prscavone@gmail.com>
2020-05-21 12:29:20 -0300

    Adding pattern recipe drawer

---
+++ Assets/InventorySystem/Editor/Receipe/PatternRecipeDrawer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(PatternRecipe))]
+public class PatternRecipeDrawer : PropertyDrawer
+{
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return 18f * 1;
+    }
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.PropertyField(position, property);
+
+        var buttonRect = new Rect(position.x, position.y, 170, position.height);
+        if (property.objectReferenceValue != null)
+        {
+            if (GUI.Button(buttonRect, new GUIContent("Open Editor of " + label.text)))
+            {
+                PatternRecipeEditorWindow.Open(property.objectReferenceValue as PatternRecipe);
+            }
+        }
+
+        //base.OnGUI(position, property, label);
+    }
+}
