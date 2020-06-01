f1e54ad5bbe8a8d07ffbaf29cb2f24e8fbef3ba0
Gabriel Pasquale <gabriel.prscavone@gmail.com>
2020-06-01 19:27:50 -0300

    Changing routes

--- Assets/InventorySystem/Scripts/Inventory/Inventory.cs
+++ Assets/InventorySystem/Scripts/Inventory/Inventory.cs
@@ -1,8 +1,7 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.ExceptionServices;
+using UnityEngine;
 
 namespace UniversalInventorySystem
 {
