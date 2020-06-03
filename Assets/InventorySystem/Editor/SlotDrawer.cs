b6bdfd3db034227bba493c17d93fd111675702fe
Gabriel Pasquale <gabriel.prscavone@gmail.com>
2020-06-03 15:42:40 -0300

    Adding Multiple variable assign to whitelist

--- Assets/InventorySystem/Editor/SlotDrawer.cs
+++ Assets/InventorySystem/Editor/SlotDrawer.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
 using UnityEditor;
 using UnityEditor.MemoryProfiler;
 using UnityEngine;
+using UnityEngine.Experimental.AI;
 using UniversalInventorySystem;
 
 [CustomPropertyDrawer(typeof(Slot))]
 public class SlotDrawer : PropertyDrawer
 {
     public Dictionary<string, SlotInfo> unfold = new Dictionary<string, SlotInfo>();
+
+    List<Object> objs;
 
     public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
     {
@@ -25,7 +31,7 @@ public class SlotDrawer : PropertyDrawer
         label.text = "Slot " + t[t.Length - 1];
 
         if (!unfold.ContainsKey(property.propertyPath))
-             unfold.Add(property.propertyPath, new SlotInfo(false, 1));
+             unfold.Add(property.propertyPath, new SlotInfo(false, 1, false, 0));
 
         var originalPosition = position;
         position = EditorGUI.PrefixLabel(position, label);
@@ -35,12 +41,108 @@ public class SlotDrawer : PropertyDrawer
 
         if (unfold[property.propertyPath].boolValue)
         {
-            var foldPos = new Rect(position.x, position.y + 16f, position.width, position.height);
-            EditorGUI.LabelField(foldPos, property.propertyPath);
-            unfold[property.propertyPath].fieldAmount = 2;
+            Rect ampos = new Rect(originalPosition.x, originalPosition.y + 18f, 120, 18);
+            bool amBool = EditorPrefs.GetBool(property.propertyPath + "AM", false);
+
+            bool amTmp = EditorGUI.Toggle(ampos, "Assign multiple", amBool);
+
+            //Take out from editorPrefs
+            if (amTmp != amBool) EditorPrefs.SetBool(property.propertyPath + "AM", amTmp);
+
+            unfold[property.propertyPath].fieldAmount = 2.5f;
+            var whitelistProp = property.FindPropertyRelative("whitelist");
+            if (amBool)
+            {
+                var foldPos = new Rect(position.x, position.y + 18f, position.width, position.height);
+
+                bool useItemAsset = EditorPrefs.GetBool(property.propertyPath, true);
+
+                Rect uia = new Rect(foldPos.x, foldPos.y, 120, foldPos.height);
+                bool tmp = EditorGUI.Toggle(uia, "Use ItemAsset", useItemAsset);
+                foldPos.x += 120;
+
+                if (tmp != useItemAsset) EditorPrefs.SetBool(property.propertyPath, tmp);
+
+                if (tmp)
+                {
+                    Rect ias = new Rect(foldPos.x, foldPos.y, 80, foldPos.height);
+                    unfold[property.propertyPath].iasExpand = EditorGUI.Foldout(ias, unfold[property.propertyPath].iasExpand, "Item Assets", true);
+                    ias.x += 80;
+                    ias.width = 160;
+
+
+                    unfold[property.propertyPath].editorAssignSize = EditorGUI.IntField(ias, "Size for assign", unfold[property.propertyPath].editorAssignSize);
+                    if (unfold[property.propertyPath].editorAssignSize < 0) unfold[property.propertyPath].editorAssignSize = 0;
+
+                    if (objs == null) objs = new List<Object>(unfold[property.propertyPath].editorAssignSize);
+                    if (objs.Count != unfold[property.propertyPath].editorAssignSize)
+                    {
+                        objs = new List<Object>(unfold[property.propertyPath].editorAssignSize);
+                        for(int i = 0; i < objs.Capacity; i++)
+                        {
+                            if (i >= objs.Count) objs.Add(null);
+                        }
+                    }
+                    if (whitelistProp.objectReferenceValue != null)
+                    {
+                        if (unfold[property.propertyPath].editorAssignSize < 1) unfold[property.propertyPath].editorAssignSize = 1;
+                        if (objs.Count >= 1) if(objs[0] == null) objs[0] = whitelistProp.objectReferenceValue;
+                    }
+
+                    if (unfold[property.propertyPath].iasExpand)
+                    {
+                        unfold[property.propertyPath].fieldAmount = 3.5f + unfold[property.propertyPath].editorAssignSize;
+
+                        for (int i = 0; i < unfold[property.propertyPath].editorAssignSize; i++)
+                        {
+                            ias.y += 18f;
+                            var objRect = new Rect(position.x + 120, ias.y, position.width - 140, ias.height);
+                            Object obj = null;
+                            if (i < objs.Count)
+                            {
+                                objs[i] = EditorGUI.ObjectField(objRect, new GUIContent($"Item Asset {i}"), objs[i], typeof(ItemAsset), false);
+                            } else
+                            {
+                                objs.Add(EditorGUI.ObjectField(objRect, new GUIContent($"Item Asset {i}"), obj, typeof(ItemAsset), false));
+                            }
+                        }
+                    }
+
+                    //Add merge btn
+                    if (amBool != amTmp)
+                    {
+                        ItemAsset newAsset = ScriptableObject.CreateInstance<ItemAsset>();
+
+                        foreach (Object iaobj in objs)
+                        {
+                            ItemAsset ia = iaobj as ItemAsset;
+                            if (ia == null) continue;
+                            foreach (Item item in ia.itemsList)
+                            {
+                                if (!newAsset.itemsList.Contains(item)) newAsset.itemsList.Add(item);
+                            }
+                        }
+
+                        if (!Enumerable.SequenceEqual((whitelistProp.objectReferenceValue as ItemAsset).itemsList, newAsset.itemsList))
+                        {
+                            whitelistProp.objectReferenceValue = newAsset;
+                        }
+                    }
+                }
+                else
+                {
+
+                }
+            } else
+            {
+                objs = new List<Object>();
+                unfold[property.propertyPath].editorAssignSize = 1;
+                var objRect = new Rect(ampos.x + 120, ampos.y, position.width - 140, ampos.height);
+                EditorGUI.ObjectField(objRect, whitelistProp, new GUIContent("Whitelist"));
+            }        
         } else
         {
-            unfold[property.propertyPath].fieldAmount = 1;
+            unfold[property.propertyPath].fieldAmount = 1;       
         }
 
         var indent = EditorGUI.indentLevel;
@@ -71,12 +173,16 @@ public class SlotDrawer : PropertyDrawer
     public class SlotInfo
     {
         public bool boolValue;
-        public int fieldAmount;
+        public bool iasExpand;
+        public float fieldAmount;
+        public int editorAssignSize;
 
-        public SlotInfo(bool _boolValues, int _fieldAmount)
+        public SlotInfo(bool _boolValues, int _fieldAmount, bool _iasExpand, int _editorAssignSize)
         {
             boolValue = _boolValues;
             fieldAmount = _fieldAmount;
+            iasExpand = _iasExpand;
+            editorAssignSize = _editorAssignSize;
         }
     }
 }
